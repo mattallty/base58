4fed2203752ebd8a5656e684c68ecfa07886199e
Matt <matt@allty.com>
2013-07-01 15:17:51 +0200

    First release

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-base58
-======
+# Base58 : A Base-58 encoder/decoder written in PHP
 
-Base58 encoder/decoder in PHP
+# Install Base58 using composer
+```
+{
+    "require" : "allty/base58"
+}
+```
+
+# Usage
+```php
+use Allty\Utils\Base58;
+
+// We will usually use something like a auto-incremented key from MySQL
+$url_id = 627868726;
+
+// encode() (shorten)
+$short = Base58::encode($url_id); // "XtZuS"
+
+// decode()
+$decoded = Base58::decode($short); // 627868726
+
+```
+
---
+++ src/Allty/Utils/Base58.php
@@ -0,0 +1,55 @@
+<?php
+namespace Allty\Utils;
+
+class Base58 {
+    
+    static $alphabet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+    
+    const BASE = 58;
+    
+    public static function encode($int) {
+        
+        if(!is_integer($int)) {
+            throw new \InvalidArgumentException('$int must be an integer. Got a '.gettype($int).'.');
+        }
+        
+        $encoded = '';
+        
+        while($int) {
+            $remainder = $int % self::BASE;
+            $int = floor($int / self::BASE);
+            $encoded = self::$alphabet{$remainder} . $encoded;       
+        }
+        
+        return $encoded;
+    }
+    
+    public static function decode($str) {
+        
+        if(!is_string($str)) {
+            throw new \InvalidArgumentException('$str must be a string. Got a '.gettype($str).'.');
+        }
+        
+        $decoded = 0;
+        
+        while($str) {
+            
+            if(($alphabetPosition = strpos(self::$alphabet, $str[0])) === false) {
+                throw new \RuntimeException('decode() cannot find "'.$str[0].'" in alphabet.');
+            }
+            
+            $decoded += $alphabetPosition * (pow(self::BASE, strlen($str) - 1));
+            $str = substr($str, 1);
+        }
+        
+        return $decoded;
+    }
+    
+}
+
+
+$num = 627868726;
+$encoded = Base58::encode($num);
+echo '$encoded : '.$encoded."\n";
+$decoded = Base58::decode($encoded);
+echo '$decoded : '.$decoded."\n";
